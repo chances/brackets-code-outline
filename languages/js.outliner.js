8eb21c62742bd49721f8f1ea7c2e863408bdd672
Will Steinmetz <willsteinmetz@me.com>
2013-10-18 01:00:38 -0400

    Added code for loading the correct parser class.

---
+++ languages/js.outliner.js
@@ -0,0 +1,19 @@
+/**
+ * @file js.outliner.js
+ * @author Will Steinmetz
+ * This file contains the functionality to generate an outline for
+ * JavaScript files.
+ */
+
+define(function (require, exports, module) {
+	"use strict";
+	
+    /**
+     * Function to parse the JavaScript file
+     */
+    function parse() {
+        
+    }
+    
+    exports.parse = parse;
+});
--- main.js
+++ main.js
@@ -2,73 +2,73 @@
 /*global define, $, brackets, window */
 
 /**
- * This extension adds a sidebar to Brackets to display an outline of the current file
+ * @file main.js
+ * @author Will Steinmetz
+ * This extension adds a panel to the right side of Brackets to outline particular
+ * code files. See the README for the supported file types.
  */
+
 define(function (require, exports, module) {
     "use strict";
     
-    var NAME = 'willsteinmetz.bracketsCodeOutline';
-	var OUTLINE_WIDTH = 250;
+    var NAME = 'willsteinmetz.bracketsCodeOutline',
+	   OUTLINE_WIDTH = 250;
 
-	var ExtensionUtils = brackets.getModule('utils/ExtensionUtils');
-	var DocumentManager = brackets.getModule('document/DocumentManager');
-	var EditorManager = brackets.getModule('editor/EditorManager');
-	var CommandManager = brackets.getModule('command/CommandManager');
-	var Menus = brackets.getModule('command/Menus');
-	var PreferencesManager = brackets.getModule('preferences/PreferencesManager');
+	var ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
+        DocumentManager = brackets.getModule('document/DocumentManager'),
+        EditorManager = brackets.getModule('editor/EditorManager'),
+        CommandManager = brackets.getModule('command/CommandManager'),
+        Menus = brackets.getModule('command/Menus'),
+        PreferencesManager = brackets.getModule('preferences/PreferencesManager');
 	
 	ExtensionUtils.loadStyleSheet(module, 'main.css');
 	
-	var preferences = PreferencesManager.getPreferenceStorage(module, { enabled: false });
-	var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+	var preferences = PreferencesManager.getPreferenceStorage(module, { enabled: false }),
+	   menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
 	
-	var currentEditor;
-	var enabled = preferences.getValue('enabled');
-	var hidden = false;
-	//var dragging = false;
-	var contentCssRight = 0;
-	var resizeInterval;
-	//var editorHeight = 0;
+	var currentEditor,
+        enabled = preferences.getValue('enabled'),
+        hidden = false,
+        contentCssRight = 0,
+        resizeInterval,
+        outliner,
+        currentType;
 	
 	enabled = (enabled !== undefined ? enabled : true);
 	
-	// hide the outline
+	/**
+	 * Hide the outline panel
+	 */
 	function hide() {
 		if (enabled) {
-			//$('#wdMinimap').hide();
 			$('#brackets-code-outline').hide();
 			$('.main-view .content').css('right', contentCssRight + 'px');
 			hidden = true;
 		}
 	}
 	
-	// show the outline
+	/*
+	 * Show the outline panel
+	 */
 	function show() {
-		//$('#wdMinimap').show();
 		$('#brackets-code-outline').show();
 		$('.main-view .content').css('right', OUTLINE_WIDTH + contentCssRight + 'px');
 		hidden = false;
 	}
 	
-	// enable the extension
+	/**
+	 * Enable the outline panel
+	 */
 	function enable() {
 		enabled = true;
 		
 		contentCssRight = parseInt($('.main-view .content').css('right'), 10);
 		$('.main-view').append('<div id="brackets-code-outline"><header>Code Outline</header><nav></nav></div>');
-		//$('.main-view').append('<div id="wdMinimap"><div class="visible-box"></div><pre></pre></div>');
 		$('.main-view .content').css('right', OUTLINE_WIDTH + contentCssRight + 'px');
 		updateListeners();
 		documentSwitch();
 		
 		resizeInterval = setInterval(function() {
-			/*if (currentEditor) {
-				if (editorHeight != $('#editor-holder').height()) {
-					editorResize();
-					editorHeight = $('#editor-holder').height();
-				}
-			}
-			if ($('#wdMinimap').css('background-color') != $('.CodeMirror').css('background-color')) setThemeColors();*/
 			if ($('#brackets-code-outline').css('background-color') != $('.CodeMirror').css('background-color')) {
 				setThemeColors();
 			}
@@ -78,88 +78,87 @@ define(function (require, exports, module) {
 		CommandManager.get(NAME + 'showOutline').setChecked(true);
 	}
 	
-	// update the listeners
+	/**
+	 * update the event listeners for the extension
+	 */
 	function updateListeners() {
 		if (enabled) {
-			/*$(DocumentManager).on('currentDocumentChange.wdMinimap', documentSwitch);
-			$(DocumentManager).on('workingSetRemove.wdMinimap', documentClose);
-			$('#wdMinimap pre, #wdMinimap .visible-box').on('mousedown.wdMinimap', visibleBoxMouseDown);
-			$(document).on('mouseup.wdMinimap', visibleBoxMouseUp);
-			$('#wdMinimap pre, #wdMinimap .visible-box').on('mousemove.wdMinimap', visibleBoxMouseMove);*/
 			$(DocumentManager).on('currentDocumentChange.bracketsCodeOutline', documentSwitch);
 			$(DocumentManager).on('workingSetRemove.bracketsCodeOutline', documentClose);
 		} else {
 			if (currentEditor) {
-                //$(currentEditor.document).off('.wdMinimap');
                 $(currentEditor.document).off('.bracketsCodeOutline');
             }
-			/*$(DocumentManager).off('.wdMinimap');
-			$(document).off('.wdMinimap');*/
 			$(DocumentManager).off('.bracketsCodeOutline');
 			$(document).off('.bracketsCodeOutline');
 		}
 	}
 	
-	// handle a document being swapped
+	/**
+	 * Handle a document being swapped
+	 */
 	function documentSwitch() {
 		if (hidden) {
             show();
         }
 		
 		if (currentEditor) {
-			//$(currentEditor.document).off('.wdMinimap');
 			$(currentEditor.document).off('.bracketsCodeOutline');
 		}
 		
 		currentEditor = EditorManager.getCurrentFullEditor();
-		//currentEditor.document.file.name << file
-		if (!currentEditor) { 
-			//$('#wdMinimap').hide();
+		if (!currentEditor) {
 			$('#brackets-code-outline').hide();
 			return;
-		}
-		else {
-			//$('#wdMinimap').show();
+		} else {
 			$('#brackets-code-outline').show();
 		}
 		
-		//$('#wdMinimap pre').css('top', 0);
 		documentEdit();
 		
-		//$(currentEditor.document).on('change.wdMinimap', documentEdit);
 		$(currentEditor.document).on('change.bracketsCodeOutline', documentEdit);
-		//$(currentEditor).on('scroll.wdMinimap', editorScroll);
 	}
 	
-	// get the current document
+	/**
+	 * Get the current document and set up the outliner class for use
+	 */
 	function documentEdit() {
-		//$('#wdMinimap pre').text(currentEditor.document.getText());
-		//editorScroll();
+		var file = currentEditor.document.file.name,
+			type = file.substr(file.lastIndexOf('.') + 1);
+		if (type && (type != currentType)) {
+            currentType = type;
+            outliner = require('languages/' + type + '.outliner.js');
+        }
 	}
 	
-	// handle a document being closed
+	/**
+	 * handle a document being closed
+	 */
 	function documentClose() {
 		if (DocumentManager.getWorkingSet().length == 0) {
             hide();
         }
 	}
 	
-	// disable the extension
+    /**
+	 * Disable the extension
+	 */
 	function disable() {
 		enabled = false;
 		
-		//$('#wdMinimap').remove();
 		$('#brackets-code-outline').remove();
 		$('.main-view .content').css('right', contentCssRight + 'px');
-		/*updateListeners();
+		updateListeners();
 		
-		clearInterval(resizeInterval);*/
+		clearInterval(resizeInterval);
 		
 		preferences.setValue('enabled', false);	
 		CommandManager.get(NAME + 'showOutline').setChecked(false);
 	}
 	
-	// toggle the outline's state
+	/**
+	 * Toggle the outline panel's state
+	 */
 	function toggle() {
 		if (!enabled) {
             enable();
@@ -168,18 +167,16 @@ define(function (require, exports, module) {
         }
 	}
 	
-	// set the colors for the outline
+	/**
+	 * Set the colors for the outline based on the editor's theme
+	 */
 	function setThemeColors() {
-		//var minimap = $('#wdMinimap');
-		//var pre = $('#wdMinimap pre');
 		var editor = $('.CodeMirror');
 		
-		//minimap.css({
         $('#brackets-code-outline').css({
             'background-color': editor.css('background-color'),
             'color': editor.css('color')
 		});
-		//pre.css('color', editor.css('color'));
 	}
 	
 	// add the menu item
